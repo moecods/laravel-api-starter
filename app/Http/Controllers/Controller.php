4962be095fc495ab3a5d33f89b70626fd487e3f3
mohammadeshaghi <mohammadeshaghi1998@gmail.com>
2023-12-08 22:43:42 +0330

    feat: swagger, install and config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,34 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="mohammadeshaghi1998@gmail.com",
+ *          name="mohammad eshaghi",
+ *          url="https://backendcast.ir",
+ *     ),
+ * )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @OA\Get(
+     *     path="/api/hi",
+     *     description="Home page",
+     *     tags={"Home"},
+     *     @OA\Response(response="200", description="An example resource")
+     * )
+     */
+
+    public function index()
+    {
+        return response()->json(['hi' => 'hi']);
+    }
 }
